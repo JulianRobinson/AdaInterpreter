d2c7047fb41556f15ff3fb98a4bd491811e2f3e1
Julian Robinson <68309335+JulianRobinson@users.noreply.github.com>
2020-10-07 16:33:02 -0400

    Started getToken()
    Method getToken() scans all tokens using a Scanner and adds them to an ArrayList. Will work on formatting the output in the next commit.  Should getToken() scan one character at a time? Or take every token separated by whitespace and try to match that way?

--- Token.java
+++ Token.java
@@ -7,8 +7,11 @@
     * 10/7/2020
 */
 
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /*
     Plan is to have a enum in the Token class for all 5 categories
@@ -20,7 +23,8 @@ import java.util.Scanner;
 */
 
 
-public class Token
+
+public class Token 
 {
     Ident idType;       //enum 
     Keyword keyType;
@@ -42,7 +46,8 @@ public class Token
     enum Keyword      //enum (mini class) for keywords
     {   
         IF (101), ELSE (102), THEN (103), END (104), CONSTANT (105),
-        EXIT (106), WHILE (107), LOOP (108), FOR (109);
+        EXIT (106), WHILE (107), LOOP (108), FOR (109), NOT_REC(110), 
+        EOF(111);
         private final int code;
         Keyword(int tkCode)
         {
@@ -86,48 +91,106 @@ public class Token
 
     }
 
+    //Identifier Token Constructor
     Token(Ident type, String scanned)
     {
         idType = type;
         value = scanned;        
     }
 
+    //Keyword Token Constructor
     Token(Keyword type, String scanned)
     {
         keyType = type;
         value = scanned;
     }
 
+    //Separator Token Constructor
     Token(Separator type, String scanned)
     {
         sepType = type;
         value = scanned;
     }
 
+    //Operator Token Constructor
     Token(Operator type, String scanned)
     {
         opType = type;
         value = scanned;
     }
 
+    //Literal Token Constructor
     Token(Literal type, String scanned)
     {
         litType = type;
         value = scanned;
     }
 
-    public static void main(String[] args)
+    //returns list of tokens
+    public static List<Token> getTokens(Scanner scan)
     {
+        List<Token> tokens = new ArrayList<Token>();
+       
+        while(scan.hasNext())
+        {
+            String next = scan.next();      //each token, starting to think I should scan one character at a time
+            if(scan.hasNext() == false)     //special cases like literals
+            {
+                System.out.println("End of File");
+                tokens.add(new Token(Keyword.EOF, "End of File"));
+            }
+            //switch for everything except literals
+            switch(next)
+            {
+                //Identifiers
+                case "int":
+                    System.out.println("scan.next(): " + next);
+                    tokens.add(new Token(Ident.INT, next));
+
+                //Keywords
+                case "if":
+                    System.out.println("scan.next(): " + next);
+                    tokens.add(new Token(Keyword.IF, next));
+                //Separators
+                case "=":
+                    System.out.println("scan.next(): " + next);
+                    tokens.add(new Token(Separator.ASSIGN, next));
+                //Operators    
+                case "+":
+                    System.out.println("scan.next(): " + next);
+                    tokens.add(new Token(Operator.ADD, next));
+                case "2":
+                    System.out.println("scan.next(): " + next);
+                    tokens.add(new Token(Literal.LIT_NUM, next));
+                default:
+                System.out.println("Token not recognized: " + next);
+                tokens.add(new Token(Keyword.NOT_REC, next));
+                
+            }
+
+        }
+        return tokens;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        //Create File and Scanner
+        File input = new File("AdaInput.txt");
+        Scanner scan = new Scanner(input);
+
         //testing how to print values
+        /*
         System.out.println("First Identifier (Ident.values()[0]): " + Ident.values()[0]);
         System.out.println(String.format("%s%03d", "Using string.format to ensure 3 digits for Ident code: ", Ident.values()[0].code));
         System.out.println("First Keyword: " + Keyword.values()[0].code);
+        */
+        
+        getTokens(scan);
 
+
+        scan.close();
+        
     }
 
 }
-
-/*
-
-*/
 
